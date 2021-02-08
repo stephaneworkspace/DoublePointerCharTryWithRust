d47593b15daf1f37eb3ed65282cc26620f3ee0cb
stephaneworkspace <s.bressani@bluewin.ch>
2021-02-08 22:55:32 +0100

    OK

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use cpp::cpp;
+use std::os::raw::c_char;
+use std::ffi::CStr;
 
 cpp!{{
     #include <iostream>
@@ -23,10 +25,20 @@ fn main() {
     };
     assert_eq!(r, 42);
     unsafe {
-        cpp!([double_ptr as "const char **"] -> u32 as "int32_t" {
+        cpp!([double_ptr as "const char **"] {
             for (int i=0; i<2; i++) {
                 std::cout << "Hello Double Pointer " << double_ptr[i] << std::endl;
             }
         });
     }
+    let mut vec_string: Vec<String> = Vec::new();
+    for i in 0..2 {
+        let occurs: *const c_char = unsafe {
+            cpp!([double_ptr as "const char **", i as "int32_t"] -> *const c_char as "const char *" {
+                return double_ptr[i];
+            })
+        };
+        vec_string.push(unsafe { CStr::from_ptr(occurs).to_str().unwrap_or("???").to_string() });
+    }
+    println!("{:?}", vec_string.as_slice());
 }
